5fb4ccd5644b936f94f21a97af4904c743e74ea2
developer-Arif-Hasan <mdarifhasan7760@gmail.com>
2024-09-27 11:02:33 +0600

    full project done

--- script/alphaClash.js
+++ script/alphaClash.js
@@ -10,6 +10,76 @@
 // //  console.log(playGroundSection.classList)
 //  playGroundSection.classList.remove('hidden')
 // }
+
+// call back the function
+function handleKeyBUttonPress(event){
+    const playerPressed=event.key;
+    
+// stop the game
+   if(playerPressed == 'Escape'){
+    gameOver();
+   }
+
+
+    //    get the expected press
+       const currentAlphabetElement=document.getElementById('current-alphabet')
+            const currentAlphabet=currentAlphabetElement.innerText;
+               const expectedAlphabet=currentAlphabet.toLowerCase();
+                //   checked math or not
+                   if(playerPressed==expectedAlphabet){
+                    //  console.log('pressed math')
+                    //  start a new round
+                    // step 1 get the new score
+                    //     const currentScoreElement=document.getElementById('current-score')
+                    //         const currentScoreText=currentScoreElement.innerText;
+                    //             const currentScore=parseInt(currentScoreText)
+                    //                console.log(currentScore)
+                    // // step 2 update the score now
+                    // const newScore=currentScore + 1;
+
+                    // // step 3 show the score
+                    // currentScoreElement.innerText=newScore;
+
+
+
+                    const currentScore=getElementTextValueById('current-score')
+                    const updatedScore=currentScore + 1;
+                    setElementTExtVAlueById('current-score',updatedScore)
+
+                    removeBackgroundById(expectedAlphabet)
+                    continueGame()
+                   }
+                   else{
+                    // console.log('pressed do not match')
+
+                    //  //step 1: get the current life count
+                    //  const currentLifeElement=document.getElementById('current-life')
+                    //  const currentLifeText=currentLifeElement.innerText;
+                    //  const currentLIfe=parseInt(currentLifeText)
+                     
+                    // //  step 2: update the current life count
+                         
+                    // const newLife= currentLIfe - 1;
+
+                    // // step 3: show the life count
+
+                    // currentLifeElement.innerText=newLife
+
+                    const currentLife=getElementTextValueById('current-life')
+                    const updatedLife= currentLife - 1;
+                    setElementTExtVAlueById('current-life',updatedLife)
+
+                    if(updatedLife == 0){
+                        gameOver();
+                    }
+                   }
+
+}
+
+// capture key press
+document.addEventListener('keyup',handleKeyBUttonPress)
+
+
 function continueGame(){
     // random element gulake call kora hoiche
    const alphabet=getRandomElement();
@@ -24,6 +94,27 @@ function continueGame(){
 
 function play(){
     hideElementById('home-section')
+    hideElementById('final-score')
     showElementById('play-ground-section')
+
+    setElementTExtVAlueById('current-life',5) 
+    setElementTExtVAlueById('current-score',0)
+
     continueGame()
+}
+
+function gameOver(){
+   hideElementById('play-ground-section')
+   showElementById('final-score')
+//    update final score
+// 1get the current score
+  const lastSCore=getElementTextValueById('current-score')
+//   console.log(lastSCore);
+  setElementTExtVAlueById('game-score',lastSCore)
+
+//   clear the last selected alphabet hide
+
+const currentAlphabet=getTextElementByID('current-alphabet')
+// console.log(currentAlphabet)
+removeBackgroundById(currentAlphabet);
 }
--- script/utility.js
+++ script/utility.js
@@ -14,7 +14,10 @@ function setLightInBgById(elementID){
     element.classList.add('bg-orange-400')
 
 }
-
+function removeBackgroundById(elementId){
+    const element=document.getElementById(elementId)
+    element.classList.remove('bg-orange-400')
+}
 function getRandomElement(){
 
     // alphabet string theke alada alada index ber kora split diye
@@ -28,4 +31,25 @@ function getRandomElement(){
     // split kora alphabet gulan random index diye pass kore deya
     const alphabet=alphabets[index];
     return alphabet;
+}
+
+
+function getElementTextValueById(elementId){
+     const element=document.getElementById(elementId)
+        const elementValueText=element.innerText;
+           const value=parseInt(elementValueText)
+              return value;
+}
+
+
+function setElementTExtVAlueById(elementId,value){
+    const element=document.getElementById(elementId)
+        element.innerText=value;
+}
+
+
+function getTextElementByID(elementId){
+    const element=document.getElementById(elementId)
+      const text=element.innerText;
+        return text;
 }
